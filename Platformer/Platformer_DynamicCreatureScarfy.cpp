68493a80ed0bb04928ae8ff2137f53802f31a5f1
Guillaume Pachot <guillaume.pachot@outlook.fr>
2023-07-14 17:44:19 +0200

    scarfy a 1 pv

--- Platformer/Platformer_DynamicCreatureScarfy.cpp
+++ Platformer/Platformer_DynamicCreatureScarfy.cpp
@@ -9,8 +9,8 @@ cDynamicCreatureScarfy::cDynamicCreatureScarfy(cLevel* l) : cDynamicCreature("sc
 	fSpriteOffsetX = -12.0f;
 	fSpriteOffsetY = -12.0f;
 	bFriendly = false;
-	nHealth = 6;
-	nHealthMax = 6;
+	nHealth = 1;
+	nHealthMax = 1;
 	level = l;
 	vx = 0;
 	bSolidVsMap = true;
